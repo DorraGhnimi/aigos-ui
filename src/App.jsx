a2e83bd54177c5c5c8cb751f295ddb45b75f1b6c
DorraGhnimi <21196213+DorraGhnimi@users.noreply.github.com>
2024-09-18 23:52:51 +0100

    profiles and latches navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import './App.css'
 import {User, MessageCircle} from 'lucide-react'
 import ProfileSelector from './components/ProfileSelector';
+import MatchesList from './components/MatchesList';
+import { useState } from 'react';
 
 function App() {
+
+const [currentView, setCurrentView] = useState('ProfileSelector');
+
   return (
-    <>
-    <div className='max-w-max mx-auto p-9'>
+    <div className='max-w-screen-md mx-auto p-9' >
       <nav className='flex justify-between mb-4'>
         <div className="p-4 flex justify-center space-x-4">
-        <User/>
+          
+        <User onClick={()=>setCurrentView("ProfileSelector")}/>
         </div>
        <div  className="p-4 flex justify-center space-x-4">
-       <MessageCircle/>
+       <MessageCircle  onClick={()=>setCurrentView("MatchesList")}/>
        </div>
       </nav>
-      <ProfileSelector/>
-
+      {currentView == "ProfileSelector" &&
+        <ProfileSelector/>
+      }
+      {currentView == "MatchesList" &&
+        <MatchesList/>
+      }
     </div>
-    </>
   )
 }
 
